7b3af3d3a76fc100d209e7fee7b00a2b2d8c021a
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-10-15 16:17:55 +0000

    edit header for compiling

--- binary_trees.h
+++ binary_trees.h
@@ -10,22 +10,6 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
-{
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
-
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
 typedef struct binary_tree_s
 {
 	int n;
