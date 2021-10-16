a1fe18c08b14872555ace057314759a22c096ec5
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-10-16 21:10:29 +0000

    add task 1 files

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - inserts child node on left side
+ * @parent: parent node
+ * @value: value to store in new node
+ * Return: returns pointer to created node or NULL if failed/null parent
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = binary_tree_node(parent, value);
+
+	if (parent == NULL)
+		return (NULL);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->left == NULL)
+		parent->left = new;
+	else
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+		parent->left = new;
+		new->parent = parent;
+	}
+
+	return (new);
+}
