c7b8c7a563284fd17c7be5835d0f50f994f4ca3a
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-10-16 21:15:16 +0000

    add task 1 again? not sure why checker says compile error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,8 +12,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	binary_tree_t *new = binary_tree_node(parent, value);
 
-	if (parent == NULL)
-		return (NULL);
 	if (new == NULL)
 		return (NULL);
 
