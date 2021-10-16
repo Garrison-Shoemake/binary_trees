2e0445dd0226fafee3b9a3b4dcaa5967225cd79c
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-10-16 21:14:04 +0000

    change order of null check

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,15 +7,15 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	if (parent == NULL)
+		return (NULL);
+
 	binary_tree_t *new = binary_tree_node(parent, value);
 
 	if (parent == NULL)
 		return (NULL);
 	if (new == NULL)
-	{
-		free(new);
 		return (NULL);
-	}
 
 	if (parent->left == NULL)
 		parent->left = new;
